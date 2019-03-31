1f749898d6c1460e663b35d4694207d986d07a85
Arna Ghosh <arnatubaikgp@gmail.com>
2019-03-31 16:18:17 -0400

    Adding comments to improve understanding

--- Vehicle simulations/Field.py
+++ Vehicle simulations/Field.py
@@ -4,12 +4,15 @@ import numpy as np
 
 class Field():
 	def __init__(self,size, sources,field_order):
+		# Setup a field, create an image with zeros and populate intensity values with field strength values 
+		# sources - Set of coord of sources
+		# field_order - Order of field strength decay from a single source
 		self.img = np.zeros(np.array(size+(3,)))
 		self.field_order = field_order
 		self.create_field(sources)
-		# print(self.img)
 
 	def radial_pattern(self, pt_x, pt_y):
+		# creates a field strength pattern for one source
 		height = self.img.shape[0]
 		width = self.img.shape[1]
 		row_arr = np.array([np.linspace(0,height-1,height)]*width).T
@@ -21,15 +24,18 @@ class Field():
 		return np.repeat(radial_pattern_arr[:,:,np.newaxis],3,axis=2)
 
 	def create_field(self, sources):
+		# creates additive field strength pattern of all sources
 		for s in range(len(sources)):
 			self.img = self.img + self.radial_pattern(sources[s,0],sources[s,1])
 
 	def display_field(self):
+		# display the field strength as an image
 		cv2.imshow("Field",self.img)
 		cv2.waitKey(0)
 
 
 if __name__ =='__main__':
+	# Testing functions and how to initiate Field object example
 	background = Field((200,200),np.random.randint(20,180,size=(10,2)),2)
 	background.display_field()
 	
